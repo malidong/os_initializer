54ad4c6cce61ab803e4e58a40a14e6e001285eaf
lidong-ma <lidong.ma@dena.com>
2018-06-22 19:51:39 +0900

    aria2 config

--- deploy.sh
+++ deploy.sh
@@ -8,9 +8,12 @@
 
 WORK_DIR=$(cd `dirname $0`; pwd) 
 
+# vim config
 mkdir -p ~/.vim/autoload ~/.vim/bundle && \
 curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
-
 git clone https://github.com/scrooloose/nerdtree.git ~/.vim/bundle/nerdtree
-
 cp -R ${WORK_DIR}/vim_conf/* ~/
+
+# aria2 config
+mkdir -p ~/.aria2
+cp -R ${WORK_DIR}/aria2_conf/* ~/.aria2/ 
